d8c5ede7fee7f6ccb26d557bd0c78625e512a3af
IvanBulatov <bulatov@informatics.ru>
2024-05-28 17:11:13 +0300

    вообще другой сервер
    выфвыыфв

--- index.js
+++ index.js
@@ -1,55 +1,38 @@
-let express = require(`express`);
+let express = require('express');
 let app = express();
-let cors = require('cors')
 let port = 3005;
-app.listen(port, function () {
-    console.log(`Сервер запущен: http://localhost:${port}`)
-});
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('NEpublic'));
+let dayjs = require('dayjs');
+let relativeTime = require('dayjs/plugin/relativeTime');
+require('dayjs/locale/ru');
+dayjs.locale('ru');
+dayjs.extend(relativeTime)
 
-
-
-// Настройка БД
-let mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/kinoticks');
-
-
-// Схема и модель
-let filmSchema = new mongoose.Schema({
-    title: String,
-    preview: String,
-    rating: Number,
-    description: String
-}, {
-    timestamps: true
+app.listen(port,function(){
+    console.log(`http://localhost:${port}`);
 })
 
-let Film = mongoose.model('film', filmSchema);
+let cors = require('cors');
+app.use(cors());
 
-// Роуты
+app.use(express.static('public'));
 
-// Поиск всех фильмов
-app.get(`/films`, async function (req, res) {
-    let title = req.query.title;
-    let search = {}; 
+let card = [{
+    "image":"Res3",
+    "title":"Natural Plants",
+    "gold": 1400
+},
+{
+    "image":"Res2",
+    "title":"Artificial Plants",
+    "gold":900
+},
+{
+    "image":"Res1",
+    "title":"Artificial Plants",
+    "gold":3500
+}];
 
-    if (title) {
-        search.title = title;
-    }
-
-    let data = await Film.find(search).sort({rating: -1});
-    res.send(data)
-});
-
-
-// Поиск всех данных одного фильма
-app.get(`/film`, async function (req, res) {
-    let id = req.query.id;
-    let data = await Film.findOne({_id: id});
-    res.send({
-        film: data
-    })
+app.get('/card',async function(req,res){
+    res.send(card);
 });
